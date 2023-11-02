77b995f9e8495cf87339a793c3009bf9f088951f
Yarismil <81661265+Yarismil@users.noreply.github.com>
2023-11-01 22:31:50 -0400

    PRATICA
    ANA TORRES 1-19-7609

---
+++ PRATICA/CONJUNTO DE NUMEROS.py
@@ -0,0 +1,7 @@
+#Crea un conjunto con los números del 1 al 5. Luego añade el número 4 y muestra el conjunto.
+
+conjunto_numeros = {1, 2, 3, 4, 5}
+print(conjunto_numeros)
+conjunto_numeros.add(4)
+
+print("Numero agregado\n", conjunto_numeros)
